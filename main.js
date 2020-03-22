9c269fc06054373323671b7ccf8351f9ca85fd85
oodoow <oodoow@gmail.com>
2020-03-22 17:56:19 +0100

    better selectors for data

--- main.js
+++ main.js
@@ -3,63 +3,65 @@ const Apify = require('apify');
 //const { log } = Apify.utils;
 const sourceUrl = 'https://www.gov.uk/government/publications/covid-19-track-coronavirus-cases';
 //const LATEST = 'LATEST';
-//let check = false;
 
 Apify.main(async () =>
 {
-
-    const input = {
-        email: 'katacek@gmail.com'
-    }
-        
-    //const { email } = await Apify.getValue('INPUT');
-    const email = input.email
-    
-    //why opening request queue if I have just one page to scrape?
-    //const requestQueue = await Apify.openRequestQueue();
-
-    // how to open existing KVS?
-    //const kvStore = await Apify.openKeyValueStore('COVID-19-POLAND');
-    //const dataset = await Apify.openDataset('COVID-19-POLAND-HISTORY');
-
     console.log('Launching Puppeteer...');
     const browser = await Apify.launchPuppeteer();
 
     const page = await browser.newPage();
+    await Apify.utils.puppeteer.injectJQuery(page);
 
     await page.goto('https://www.gov.uk/government/publications/covid-19-track-coronavirus-cases');
     
     const trackCoronavirusCases = '#attachment_4077017 > div.attachment-details > h2 > a';
     await page.waitForSelector(trackCoronavirusCases);
 
-    const trackCoronavirusCasesLink = await page.$eval(trackCoronavirusCases,el=>el.href);
-    await page.goto(trackCoronavirusCasesLink);
+    const trackCoronavirusCasesLink = await page.$eval(trackCoronavirusCases, el => el.href);
+    // networkidle0 : wait for all page to load
+    await page.goto(trackCoronavirusCasesLink), { waitUntil: 'networkidle0', timeout: 60000 };
+   
+    await page.waitForSelector('div.flex-fluid');
+    await page.waitFor(4000);
+ 
+    // page.evaluate(pageFunction[, ...args]), pageFunction <function|string> Function to be evaluated in the page context, returns: <Promise<Serializable>> Promise which resolves to the return value of pageFunction
+    const result = await page.evaluate(() => {
 
-    const totalCases = '#ember474 > svg > g.responsive-text-label > svg > text'
-    const dailyConfirmed = '#ember475 > svg > g.responsive-text-label > svg > text'
-    const patientsRecovered = '#ember469 > svg > g.responsive-text-label > svg > text'
-    const deceased = '#ember476 > svg > g.responsive-text-label > svg > text' 
+        const getInt = (x)=>{
+            return parseInt(x.replace(' ','').replace(',',''))};
+        const now = new Date();
+        
+        // eq() selector selects an element with a specific index number, text() method sets or returns the text content of the selected elements
+        const totalInfected = $("text[vector-effect='non-scaling-stroke']").eq(0).text();
+        const dailyConfirmed = $("text[vector-effect='non-scaling-stroke']").eq(1).text();
+        const patientsRecovered = $("text[vector-effect='non-scaling-stroke']").eq(2).text();
+        const deceased = $("text[vector-effect='non-scaling-stroke']").eq(3).text();
+        const england = $("text[vector-effect='non-scaling-stroke']").eq(4).text();
+        const scottland = $("text[vector-effect='non-scaling-stroke']").eq(5).text();
+        const wales = $("text[vector-effect='non-scaling-stroke']").eq(6).text();
+        const ireland = $("text[vector-effect='non-scaling-stroke']").eq(7).text();
+        const data = {
+            totalInfected: getInt(totalInfected),
+            dailyConfirmed: getInt(dailyConfirmed),
+            recovered: getInt(patientsRecovered),
+            deceased: getInt(deceased),
+            england: getInt(england),
+            scottland: getInt(scottland),
+            wales: getInt(wales),
+            ireland: getInt(ireland),
+            sourceUrl:'https://www.gov.uk/government/publications/covid-19-track-coronavirus-cases',
+            lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString()
+            //readMe: 'https://apify.com/vaclavrut/covid-pl',
+        };
+        return data;
+    });       
+    console.log(result)
 
-    const textTotal = await page.$eval(totalCases, el => el.textContent);
-    const textDaily = await page.$eval(dailyConfirmed, el => el.textContent);
-    const textRecovered = await page.$eval(patientsRecovered, el => el.textContent);
-    const textDeceased = await page.$eval(deceased, el => el.textContent);
-
-    const data = {
-        total: textTotal,
-        today: textDaily,
-        recovered: textRecovered,
-        deceased: textDeceased,
-        sourceUrl,
-        // to be added later
-        //lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
-        //readMe: 'https://apify.com/vaclavrut/covid-pl',
-    };
-    console.log(data)
+       
 });
 
-// Run the crawler and wait for it to finish.
-//await crawler.run();
+
+
 
 
 
@@ -69,7 +71,7 @@ Apify.main(async () =>
 
             
 
-            // Compare and save to history
+            // //Compare and save to history
             // const latest = await kvStore.getValue(LATEST);
             // delete latest.lastUpdatedAtApify;
             // const actual = Object.assign({}, data);
@@ -93,18 +95,4 @@ Apify.main(async () =>
         // },
     
 
-    // if there are no region data, send email, because that means something is wrong
-    // const env = await Apify.getEnv();
-    // if (check) {
-    //     await Apify.call(
-    //         'apify/send-mail',
-    //         {
-    //             to: email,
-    //             subject: `Covid-19 PL from ${env.startedAt} failed `,
-    //             html: `Hi, ${'<br/>'}
-    //                     <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
-    //                     run had 0 regions, check it out.`,
-    //         },
-    //         { waitSecs: 0 },
-    //     );
-    // }
+    
